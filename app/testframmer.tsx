a06a4239391c53d59d7d70e67bd04eccb6a44f5a
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-05-20 18:39:34 +0530

    learn motion.div

---
+++ app/testframmer.tsx
@@ -0,0 +1,28 @@
+'use client'
+import {motion} from 'framer-motion'
+
+
+
+const TestFrammer=()=>{
+    const li=[];
+    for(let i=0;i<10;i++){
+        i&1?li.push(0):li.push(100);
+    }
+
+    return (
+        <div>
+            <motion.div className="h-20 w-96 bg-amber-500 rounded-lg ml-20 " animate={{ x:li }} transition={{ ease: "easeInOut", duration: 0.5 , }} 
+  whileHover={{
+    scale: 1.2,
+    transition: { duration:0.5, },
+    
+  }}
+  
+  
+
+  whileInView={{ opacity: 1 }}  />
+        </div>
+    );
+}
+
+export default TestFrammer;
